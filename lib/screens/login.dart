eec44bf15ed0590a0eba5342bef530ff74da90e1
This Laptop <mymac@MacBook-Pro.local>
2023-10-24 23:29:38 +0700

    update

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,11 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flatter_project/controllers/authControllers.dart';
-import 'package:flatter_project/routes/routeName.dart';
-import 'package:flatter_project/widget/dialogCustom/customDialog.dart';
 import 'package:flatter_project/widget/loginButton.dart';
 import 'package:flatter_project/widget/textField.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 
 class Login extends StatefulWidget {
@@ -21,47 +17,6 @@ class _LoginState extends State<Login> {
   final _password = TextEditingController();
 
   final authC = Get.find<AuthController>();
-
-  // void _login(BuildContext context) async {
-  //   String email = _email.text.trim();
-  //   String password = _password.text.trim();
-
-  //   // try {
-  //   //   UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-  //   //     email: email,
-  //   //     password: password,
-  //   //   );
-  //   //   User? user = userCredential.user;
-  //   //   print('Logged in as: ${user?.email}');
-  //   //   Fluttertoast.showToast(
-  //   //     msg: 'Login berhasil',
-  //   //     gravity: ToastGravity.TOP,
-  //   //   );
-  //   //   Get.offAllNamed(Routename.home);
-  //   // } on FirebaseAuthException catch (e) {
-  //   //   if (e.code == 'invalid-email') {
-  //   //     showDialog(
-  //   //       context: context,
-  //   //       builder: (BuildContext context) {
-  //   //         return CustomAlertDialog(
-  //   //             content: 'Email tidak valid \nHarap masukkan email yang valid.',
-  //   //             iconTitle: Icons.warning);
-  //   //       },
-  //   //     );
-  //   //   } else if (e.code == 'user-not-found' ||
-  //   //       e.code == 'wrong-password' ||
-  //   //       e.toString().contains('INVALID_LOGIN_CREDENTIALS')) {
-  //   //     showDialog(
-  //   //       context: context,
-  //   //       builder: (BuildContext context) {
-  //   //         return CustomAlertDialog(
-  //   //             content: 'Email atau kata sandi salah \nCoba lagi.',
-  //   //             iconTitle: Icons.warning);
-  //   //       },
-  //   //     );
-  //   //   }
-  //   // }
-  // }
 
   @override
   Widget build(BuildContext context) {
